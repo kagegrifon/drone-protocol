dae53c5bf7ca5ef2c89025b81248350553caf30e
Grigorii Veinin <kage.grifon@yandex.ru>
2026-06-10 12:02:41 +0500

    config: make playwright fullyParallel: true

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
-  fullyParallel: false,
+  fullyParallel: true,
   reporter: 'list',
   use: {
     baseURL: 'http://localhost:5174',
